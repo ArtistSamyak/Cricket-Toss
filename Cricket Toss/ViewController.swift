adc4524639b4146127d451add7648e194e4b9adf
ArtistSamyak <samyak.professional@gmail.com>
2020-06-28 14:20:00 +0530

    Initial Commit

---
+++ Cricket Toss/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cricket Toss
+//
+//  Created by Samyak Pawar on 28/06/20.
+//  Copyright © 2020 ArtistSamyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
